b58fa1320deb0476c9474648d3436120ddfd7e2d
Lauren McCoy <lmccoy8911@gmail.com>
2019-04-06 21:43:33 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram 
+  
+    attr_accessor :words 
+    
+    def initialize(word)
+      @words = words 
+      
+    end 
+
+    def match 
+      @words.collect do |word|
+        @words.split("")
+        if words == word 
+          word 
+        else 
+          false 
+        end 
+      end   
+    end 
+
+end 
